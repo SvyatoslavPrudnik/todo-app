c78f1d4ca249cc406d4026b6625d8a482af1634a
Svyatoslav Prudnik <prudnik12015@gmail.com>
2021-02-05 22:15:28 +0200

    Created an implementation of important records

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,20 +3,37 @@ import './post-list-item.css'
 
 
 export default class PostListItem extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state = {}   
+        this.state = {
+            important: false
+        }
+        this.onImportant = this.onImportant.bind(this);   
+    }
+
+    onImportant(){
+        this.setState(({important}) => ({
+            important: !important
+        }))
     }
 
     render() {
         const {label} = this.props;
+        const {important} = this.state;
+        let importantClass = 'app-list-item d-flex justify-content-between';
+
+        if (important){
+            importantClass += ' important'
+        }
+
         return(
-            <div className='app-list-item d-flex justify-content-between'>
+            <div className={importantClass}>
                 <span className='app-list-item-label'>
                     {label}
                 </span>
                 <div className='d-flex justify-content-center align-items-center'>
-                    <button type='button' className='btn-star btn-sm'>
+                    <button type='button' className='btn-star btn-sm'
+                    onClick={this.onImportant}>
                         <i className='fa fa-star'></i>
                     </button>
                     <button type='button' className='btn-checkmark btn-sm'>
